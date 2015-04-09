61b1dc59000cd5ed4fbb26807ee35f2c92ca87fd
Celina Osorio Ochoa <chelinoska@gmail.com>
2015-04-09 01:04:46 -0500

    Cambio de nombre del archivo ui en código de interfaz

--- interfaces/gtk-crear-sesion.c
+++ interfaces/gtk-crear-sesion.c
@@ -23,7 +23,7 @@ main (int   argc,
 
   /* Construct a GtkBuilder instance and load our UI description */
   builder = gtk_builder_new ();
-  gtk_builder_add_from_file (builder, "builder.ui", NULL);
+  gtk_builder_add_from_file (builder, "builder-crear-sesion.ui", NULL);
 
   /* Connect signal handlers to the constructed widgets. */
   window = gtk_builder_get_object (builder, "window");
